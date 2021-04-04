f5d7272e4258cb189ceb3c566757d08214b2038f
Hasan Muşluoğlu <44349289+MusluogluHasan@users.noreply.github.com>
2021-04-04 23:28:32 +0300

    a

---
+++ Otobus1/Controllers/AccountController.cs
@@ -0,0 +1,86 @@
+﻿using Otobus1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Otobus1.Controllers
+{
+    public class AccountController : Controller
+    {
+        BiletSatısEntities db = new BiletSatısEntities();
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+        public ActionResult Ticket()
+        {
+
+            return View();
+        }
+        /*[HttpPost]
+        public ActionResult Ticket(Bilet b)
+        {
+            var bilgi = db.Bilet.FirstOrDefault(x => x.Ad == b.Ad && x.Aciklama == b.Aciklama);
+
+            if (bilgi != null)
+            {
+                Session["Ad"] = bilgi.Ad.ToString();
+                Session["Aciklama"] = bilgi.Aciklama.ToString();
+                return RedirectToAction("Ticket", "Account");
+            }
+            else
+            {
+                ViewBag.hata = "Kullanıcı Adı Veya Şifre Hatalı.";
+            }
+            return View();
+        }*/
+        public ActionResult logOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Account");
+        }
+        [HttpPost]
+        public ActionResult Login(Kullanici p)
+        {
+            var bilgiler = db.Kullanici.FirstOrDefault(x => x.Email == p.Email && x.Parola == p.Parola);
+
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.Email, false);
+                Session["Email"] = bilgiler.Email.ToString();
+                Session["Ad"] = bilgiler.Ad.ToString();
+                Session["Soyad"] = bilgiler.Soyad.ToString();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.hata = "Kullanıcı Adı Veya Şifre Hatalı.";
+            }
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Kullanici data)
+        {
+            db.Kullanici.Add(data);
+            data.Rol = "U";
+            db.SaveChanges();
+            return RedirectToAction("Login", "Account");
+        }
+
+    }
+}
---
+++ Otobus1/Controllers/BiletController.cs
@@ -0,0 +1,57 @@
+﻿using Otobus1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Otobus1.Controllers
+{
+    public class BiletController : Controller
+    {
+        // GET: Bilet
+        BiletSatısEntities db = new BiletSatısEntities();
+        [Authorize]
+        public ActionResult Index()
+        {
+            var list = db.Bilet.ToList();
+            return View(list);
+        }
+
+        [Authorize(Roles = "A")]
+        public ActionResult Ekle()
+        {
+
+            List<SelectListItem> deger1 = (from x in db.Kategori.ToList()
+
+                                           select new SelectListItem
+                                           {
+                                               Text = x.Ad,
+                                               Value = x.Id.ToString()
+                                           }).ToList();
+            ViewBag.ktgr = deger1;
+            return View();
+        }
+        [Authorize(Roles = "A")]
+        [HttpPost]
+        public ActionResult Ekle(Bilet ekle)
+        {
+            db.Bilet.Add(ekle);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [Authorize(Roles = "A")]
+        public ActionResult Sil(int id)
+        {
+            var urun = db.Bilet.Find(id);
+            db.Bilet.Remove(urun);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        
+
+    }
+}
